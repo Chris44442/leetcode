799f5c1cf4f05d5e6fd56c20f66f2391fb43dbc0
christoph bothe <christoph_bothe@gmx.de>
2023-10-12 21:39:16 +0200

    minigrep fun

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 pub fn main() {
   let args: Vec<String> = env::args().collect();
@@ -10,16 +11,19 @@ pub fn main() {
     process::exit(1);
   });
 
-
   println!("Searching for {}", config.query);
-
   println!("In file {}", config.filename);
+  
+  if let Err(e) = run(config) {
+    println!("Application error: {}", e);
+    process::exit(1);
+  }
+}
 
-  let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file");
-
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let contents = fs::read_to_string(config.filename)?;
   println!("With text:\n {}", contents);
+  Ok(())
 }
 
 struct Config {
@@ -32,8 +36,6 @@ impl Config {
     if args.len() < 3 {
       return Err("not enough arguments");
     }
-
-
     let query = args[1].clone();
     let filename = args[2].clone();
     Ok(Config {query, filename})
