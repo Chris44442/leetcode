f579df4da9c5c6a8491dbbb848d6914fbfd45b7f
christoph bothe <christoph_bothe@gmx.de>
2023-12-10 00:27:23 +0100

    aoc stuff

--- AoC/aoc_23_05/src/main.rs
+++ AoC/aoc_23_05/src/main.rs
@@ -28,8 +28,7 @@ fn parse_input() -> Vec<Vec<Vec<u64>>> {
     numbers
 }
 
-fn part1_solution() -> u64 {
-    let num = parse_input();
+fn part1_solution(num:Vec<Vec<Vec<u64>>>) -> u64 {
     let mut destination : Vec<Vec<u64>> = vec![vec![0; num.len()]; num[0][0].len()];
     for i in 0..num[0][0].len() {
         destination[i][0] = num[0][0][i];
@@ -45,7 +44,44 @@ fn part1_solution() -> u64 {
     destination.iter().map(|row| row[7]).min().unwrap()
 }
 
+fn part2_solution(num:Vec<Vec<Vec<u64>>>) -> u64 {
+    let mut result : u64 = 0;
+    for i in 0..u64::MAX {
+        let mut source : Vec<u64> = vec![0;8];
+        source[7] = i;
+        for j in (0..7).rev() {
+            source[j] = source[j+1];
+            for k in 0..num[j+1].len() {
+                if source[j+1] >= num[j+1][k][0] && source[j+1] < num[j+1][k][0] + num[j+1][k][2] {
+                    source[j] = source[j+1] - num[j+1][k][0] + num[j+1][k][1];
+                }
+            }
+        }
+        if source[0] >= num[0][0][0] && source[0] < num[0][0][0]+num[0][0][1] 
+        || source[0] >= num[0][0][2] && source[0] < num[0][0][2]+num[0][0][3] 
+        || source[0] >= num[0][0][4] && source[0] < num[0][0][4]+num[0][0][5] 
+        || source[0] >= num[0][0][6] && source[0] < num[0][0][6]+num[0][0][7] 
+        || source[0] >= num[0][0][8] && source[0] < num[0][0][8]+num[0][0][9] 
+        || source[0] >= num[0][0][10] && source[0] < num[0][0][10]+num[0][0][11] 
+        || source[0] >= num[0][0][12] && source[0] < num[0][0][12]+num[0][0][13] 
+        || source[0] >= num[0][0][14] && source[0] < num[0][0][14]+num[0][0][15] 
+        || source[0] >= num[0][0][16] && source[0] < num[0][0][16]+num[0][0][17] 
+        || source[0] >= num[0][0][18] && source[0] < num[0][0][18]+num[0][0][19] 
+        {
+            result = source[7];
+            break;
+        }
+    }
+    result
+}
+
 fn main() {
-    println!("part1 minimum: {}", part1_solution());
+    let num = parse_input();
+    println!("part1 minimum: {}", part1_solution(num));
+
+    let num = parse_input();
+    println!("part2 minimum: {}", part2_solution(num));
+
+    // println!("part2 minimum: {}", part2_solution());
 }
 
