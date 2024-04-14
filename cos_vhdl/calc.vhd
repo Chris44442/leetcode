fccc7e4047a50843d1ba2faa9705b3fe2f9a0dea
christoph bothe <hanz_wurzt@web.de>
2024-04-14 22:41:45 +0200

    calc stuff

---
+++ cos_vhdl/calc.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.fixed_pkg.all;
+
+entity calc is
+port(
+    clk : in std_logic;
+    reset : in std_logic
+);
+end entity;
+
+architecture rtl of calc is
+begin
+
+process(all) begin
+  if rising_edge(clk) then
+  end if;
+end process;
+
+end architecture;
+
