2444bac678debf04524db20fcadfa3308ec250e9
christoph bothe <hanz_wurzt@web.de>
2025-01-19 23:55:30 +0100

    more radar stuff

--- oort/src/lib.rs
+++ oort/src/lib.rs
@@ -4,6 +4,7 @@ use oort_api::prelude::*;
 
 const BULLET_SPEED: f64 = 1000.0; // m/s
 const FIGHTER_MAX_TORQUE: f64 = 2.0 * PI; // m/s^2
+const FIGHTER_MAX_ACC: f64 = 60.0; // m/s^2
 
 #[derive(Default, PartialEq, Eq)]
 pub enum RadarMode {
@@ -25,6 +26,7 @@ pub struct Contact {
     class: Class,
     position: Vec2,
     velocity: Vec2,
+    acc_to_intercept: Vec2,
     trq_to_intercept: f64,
     suggest_fire_cmd: bool,
     engaged: bool,
@@ -66,6 +68,7 @@ impl Ship {
                 class: contact.class,
                 position: contact.position,
                 velocity: contact.velocity,
+                acc_to_intercept: vec2(0.0, 0.0),
                 trq_to_intercept: 0.0,
                 suggest_fire_cmd: false,
                 engaged: true,
@@ -73,14 +76,11 @@ impl Ship {
 
             if self.radar.contact.is_empty() {
                 self.radar.contact.push(n_contact);
+            } else {
+                self.radar.contact[0] = n_contact;
             }
             self.radar.mode = RadarMode::STT;
-            //self.radar.azimuth = angle_diff(heading(), contact.position.angle() - position().angle());
-
-            //self.radar.azimuth = (self.radar.contact[0].position - position()).angle();
-            self.radar.azimuth = (contact.position - position()).angle();
-
-            //draw_line(position(), contact.position, 0x00ff00);
+            self.radar.azimuth = (self.radar.contact[0].position - position()).angle();
             let dp = contact.position - position();
             let t_start = dp.length() / BULLET_SPEED;
             //let a = contact.velocity - self.velocity;
@@ -94,17 +94,45 @@ impl Ship {
             //draw_line(position(), posi_to_shot2, 0xffff00);
 
             let safety_factor = 1.03;
-            let breaking_distance =
+            let ang_breaking_distance =
                 0.5 * angular_velocity() * angular_velocity() / FIGHTER_MAX_TORQUE * safety_factor;
 
-            //let abc = (dp[0] * dp[0] + dp[1] * dp[1]).sqrt();
+            let distance = (dp2[0] * dp2[0] + dp2[1] * dp2[1]).sqrt();
+            let v_diff = contact.velocity - velocity();
+            debug!("v_diff: {}", v_diff);
+            //let position_angle = (dp2[1] / dp2[0]).atan();
+            let v = (v_diff[0] * v_diff[0] + v_diff[1] * v_diff[1]).sqrt();
+            let v_rad = ang_diff.cos() * v;
+            debug!("v_rad: {}", v_rad);
 
-            debug!("ang_diff: {}", ang_diff);
-            debug!("ang_velocity: {}", angular_velocity());
+            let offset = 100.0; // meter
+            let breaking_distance =
+                0.5 * v_rad * v_rad / FIGHTER_MAX_TORQUE * safety_factor + offset;
+            let k3 = signum(distance - breaking_distance);
+            let k4;
+
+            if v_rad < 0.0 {
+                k4 = 1.0;
+            } else {
+                k4 = k3;
+            }
+
+            self.radar.contact[0].acc_to_intercept =
+                vec2(heading().cos(), heading().sin()).normalize() * FIGHTER_MAX_ACC * k4;
+
+            //let rel_vel = contact.velocity - velocity();
+
+            debug!(
+                "acc_to_intercept: {}",
+                v_diff.normalize() * FIGHTER_MAX_ACC * k4
+            );
+            debug!("k4: {}", k4);
             debug!("breaking_distance: {}", breaking_distance);
+            debug!("distance: {}", distance);
+
             let s1 = signum(ang_diff);
             let s2 = signum(angular_velocity());
-            let s3 = signum(abs(ang_diff) - breaking_distance);
+            let s3 = signum(abs(ang_diff) - ang_breaking_distance);
             let s4;
 
             if s1 != s2 {
@@ -115,13 +143,11 @@ impl Ship {
                 s4 = s1 * s3;
             }
 
-            if abs(ang_diff) > 0.000 {
-                let trq = FIGHTER_MAX_TORQUE * s4; // TODO fuel considerations
-                                                   //debug!("torque: {}", trq);
-                torque(trq);
-            }
+            self.radar.contact[0].trq_to_intercept = FIGHTER_MAX_TORQUE * s4;
             if abs(ang_diff) < 0.03 {
-                fire(0);
+                self.radar.contact[0].suggest_fire_cmd = true;
+            } else {
+                self.radar.contact[0].suggest_fire_cmd = false;
             }
         } else {
             self.radar.mode = RadarMode::RWS;
@@ -134,15 +160,12 @@ impl Ship {
     pub fn tick(&mut self) {
         self.update_radar();
 
-        let rm;
-        if self.radar.mode == RadarMode::RWS {
-            rm = "RWS";
-        } else {
-            rm = "STT";
+        if !self.radar.contact.is_empty() {
+            torque(self.radar.contact[0].trq_to_intercept);
+            accelerate(self.radar.contact[0].acc_to_intercept);
+            if self.radar.contact[0].suggest_fire_cmd {
+                fire(0);
+            }
         }
-        debug!("{}", rm);
-
-        //torque(trq);
-        //fire(0);
     }
 }
