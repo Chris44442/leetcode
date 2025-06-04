f7bdd14ce63551ad45df7a9a0060ad6b27efeb1a
christoph bothe <hanz_wurzt@web.de>
2025-06-05 00:01:51 +0200

    working on part 2

--- AoC/a24_09/src/main.rs
+++ AoC/a24_09/src/main.rs
@@ -82,16 +82,80 @@ pub fn part2(input : &str) -> i64{
         is_data =  !is_data;
     }
 
-    let mut blocks_clone = blocks.clone();
-    for i in (0..blocks_clone.len()).rev() {
-        if blocks_clone[i] == FREE_SPACE {
-            blocks_clone.remove(i);
+    // let mut go_ahead : bool = true;
+    for bla in 0..2 {
+
+        let mut blocks_clone = blocks.clone();
+        for i in (0..blocks_clone.len()).rev() {
+            if blocks_clone[i] == FREE_SPACE {
+                blocks_clone.remove(i);
+            }
+        }
+
+        // dbg!(&blocks_clone);
+        // dbg!(&blocks);
+        //
+        // dbg!(&blocks_clone.last());
+
+        // TODO big for loop start here
+        let mut no_this_id = 0;
+        let last_id = *blocks_clone.last().unwrap();
+        let mut break_index = 0;
+        for i in (0..blocks_clone.len()).rev() {
+            if blocks_clone[i] == last_id {
+                blocks_clone.remove(i);
+                no_this_id += 1;
+                break_index = i;
+            }
+            else {
+                break;
+            }
+        }
+
+        // dbg!(no_this_id);
+
+        let mut k = 0;
+        
+        let mut start_index = 0;
+
+        for j in 0..blocks.len() {
+            if j >= break_index - 1 {
+                // TODO?
+                // we are done
+                // go_ahead = false;
+
+                break;
+            }
+
+            if blocks[j] == FREE_SPACE {
+                k += 1;
+                if k >= no_this_id {
+                    start_index = j - k + 1;
+                    break;
+                }
+            } else {
+                // free space not big enough
+                k = 0;
+            }
+        }
+        for m in start_index..start_index+k {
+            blocks[m] = blocks.pop().unwrap();
         }
     }
 
-    dbg!(blocks_clone);
-    dbg!(blocks);
-    let acc = 1;
+
+    dbg!(&blocks);
+
+
+
+
+    let mut acc = 0;
+    for i in 0..blocks.len() {
+        if blocks[i] != FREE_SPACE {
+            acc += i as i64 *blocks[i];
+        }
+    }
+
 
     return acc;
 }
@@ -108,16 +172,16 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_1() {
-        let input = "2333133121414131402";
-        assert_eq!(1928, part1(input));
-
-    }
+    // #[test]
+    // fn test_1() {
+    //     let input = "2333133121414131402";
+    //     assert_eq!(1928, part1(input));
+    //
+    // }
     #[test]
     fn test_2() {
         let input = "2333133121414131402";
-        assert_eq!(part2(input),1);
+        assert_eq!(part2(input),2858);
 
     }
 }
